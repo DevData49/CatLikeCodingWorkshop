dfc113bb10a2a4ec2b74fa121a79c2b4f8b869aa
devdata49 <debadattahembram@gmail.com>
2020-11-19 09:20:08 +0530

    Finished lesson 1 : Gameonbjects and Scripts

---
+++ Assets/1.Basics/GameObjectsAndScripts/Scripts/Clock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    const float hoursToDegrees = -30f, secondsToDegree = -6f, minutesTodegree = -6f;
+    [SerializeField]
+    Transform hoursPivot, minutesPivot, secondsPivot;
+
+    
+    private void Update()
+    {
+        
+        var  time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation = Quaternion.Euler(0, 0, hoursToDegrees * (float)time.TotalHours);
+        minutesPivot.localRotation = Quaternion.Euler(0, 0, minutesTodegree * (float)time.TotalMinutes);
+        secondsPivot.localRotation = Quaternion.Euler(0, 0, secondsToDegree * (float)time.TotalSeconds);
+    }
+}
